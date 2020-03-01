dc7e12a803bbc1e476b00b3a94ff27ab9bfd46c8
QAZ2999 <61638310+QAZ2999@users.noreply.github.com>
2020-03-01 09:19:24 +0900

    T boT1
    mie

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,10 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@bot.command()
+async def hi(ctx):
+    await ctx.send('Fuck!!')
 
 
 bot.run(token)
