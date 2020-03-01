1ad24ec8de7365917e4e07b506a1dd7cdb514142
QAZ2999 <61638310+QAZ2999@users.noreply.github.com>
2020-03-01 09:40:58 +0900

    discord-py

--- discordbot.py
+++ discordbot.py
@@ -20,9 +20,5 @@ async def ping(ctx):
 @bot.command()
 async def hi(ctx):
     await ctx.send('Fuck!!')
-
-@bot.command()
-async def ?(ctx):
-    await ctx.send('単品「？」は嫌いです。セットでお願いします。')
     
 bot.run(token)
